edb064942669bf5dd85c49ffb81bd8bbad46efed
piyush.joshi <piyush.joshi@timesinternet.in>
2019-07-16 16:56:44 +0530

    Updates

---
+++ precache-manifest.f8a5d61042ec793b8b6a314f33f26d00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378adc691616888f64afd0d22623e6a2",
+    "url": "/portfolio-piyushjoshi/index.html"
+  },
+  {
+    "revision": "c88baaa0f12827aa1f1c",
+    "url": "/portfolio-piyushjoshi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-piyushjoshi/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c88baaa0f12827aa1f1c",
+    "url": "/portfolio-piyushjoshi/static/js/main.69946b78.chunk.js"
+  },
+  {
+    "revision": "600f901ad717895eed3b",
+    "url": "/portfolio-piyushjoshi/static/js/runtime~main.8a2157d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-piyushjoshi/static/media/logo.5d5d9eef.svg"
+  }
+]);
